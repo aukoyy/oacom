0040015071c15e924c5c6dbdf501ce929ec19ea7
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2025-10-24 17:30:57 +0200

    react-jsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,21 +7,21 @@ export default function About() {
     <div className="flex justify-center">
       <main className="">
         <h1 className="text-4xl font-bold">About</h1>
-        <div className="flex flex-col lg:flex-row-reverse mt-12">
+        <div className="flex flex-col mt-12 lg:flex-row-reverse">
           <div className="relative w-full h-[400px] mb-8 lg:w-[750px] lg:h-[750px] lg:mb-0 lg:ml-32">
             <Image
               src="/profile-me-24-min.jpg"
               alt="About Me"
               fill
-              className="rounded-lg shadow-lg object-cover"
+              className="object-cover rounded-lg shadow-lg"
             />
           </div>
-          <div className="space-y-6 w-full lg:w-1/2">
+          <div className="w-full space-y-6 lg:w-1/2">
             <p>
               Hi! I'm Øyvind
             </p>
             <p>
-              I’m an AWS Cloud & Platform Engineer with a passion for building tools and infrastructure that help others move faster.
+              I’m an AWS Cloud and Platform Engineer with a passion for building tools and infrastructure that help others move faster.
             </p>
             <p>
               I design and develop secure, scalable internal platforms on AWS that make cloud development simpler, safer, and more enjoyable for engineering teams.
@@ -30,23 +30,23 @@ export default function About() {
             <section className="mt-12 space-y-8">
               <div>
               </div>
-              
+
               <div>
                 <h2 className="text-2xl font-light text-slate-700">Recent Work Experience</h2>
-                <h3 className="text-lg mt-4">Cloud Engineer</h3>
+                <h3 className="mt-4 text-lg">Cloud Engineer</h3>
                 <p>Fremtind Forsikring AS <br /><span className="text-gray-500">Aug 2024 - now</span></p>
               </div>
               <div>
-                <h2 className="text-2xl font-light text-slate-700 my-2">Certifications</h2>
-                <ul className="list-disc list-inside space-y-1">
+                <h2 className="my-2 text-2xl font-light text-slate-700">Certifications</h2>
+                <ul className="space-y-1 list-disc list-inside">
                   <li>AWS Certified Solutions Architect - Associate</li>
                   <li>AWS Certified Developer - Associate</li>
                   <li>AWS Certified Cloud Practitioner</li>
                 </ul>
               </div>
               <div>
-                <h2 className="text-2xl font-light text-slate-700 my-2">Education</h2>
-                <h3 className="text-lg mt-4">Bachelor of Informatics</h3>
+                <h2 className="my-2 text-2xl font-light text-slate-700">Education</h2>
+                <h3 className="mt-4 text-lg">Bachelor of Informatics</h3>
                 <p>Norwegian University of Science and Technology (NTNU) <br /><span className="text-gray-500">2016 - 2019</span></p>
               </div>
             </section>
