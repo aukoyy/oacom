a2daf80a3989ef492d3d00e5dd5369a3b2987e95
Øyvind Aukner <22494963+aukoyy@users.noreply.github.com>
2026-02-20 21:26:28 +0100

    add article start

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import HomeClient from './components/HomeClient';
 
 export default function Home() {
   const posts = getAllPosts();
-  console.log('All posts:', posts);
   const publishedPosts = posts.filter(post => post.publish);
 
   return <HomeClient publishedPosts={publishedPosts} />;
